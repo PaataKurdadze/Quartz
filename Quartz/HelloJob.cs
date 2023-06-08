43f1b0d1df96251be862d6f465a148a4e41cda48
Paata Kurdadze <paata2016@gmail.com>
2023-06-08 16:16:28 +0400

    Add project files.

---
+++ Quartz/HelloJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Quartz
+{
+    internal class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync($"Executing background job => {DateTime.Now}");
+        }
+    }
+}
---
+++ Quartz/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace Quartz
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder()
+                .ConfigureServices((cxt, services) =>
+                {
+                    services.AddQuartz(q =>
+                    {
+                        q.UseMicrosoftDependencyInjectionJobFactory();
+                    });
+                    services.AddQuartzHostedService(opt =>
+                    {
+                        opt.WaitForJobsToComplete = true;
+                    });
+                }).Build();
+
+            var schedulerFactory = builder.Services.GetRequiredService<ISchedulerFactory>();
+            var scheduler = await schedulerFactory.GetScheduler();
+
+            // define the job and tie it to our HelloJob class
+            var job = JobBuilder.Create<HelloJob>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+            // Trigger the job to run now, and then every 40 seconds
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(16, 13))
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(5)
+                    .RepeatForever())
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+
+            // will block until the last running job completes
+            await builder.RunAsync();
+        }
+    }
+}
